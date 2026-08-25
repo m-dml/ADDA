328332ac53a60e87bf47ab63e2d6a914c277a82c
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2026-08-25 15:11:56 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -3,22 +3,43 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import shutil
+import sys
+
+sys.path.insert(0, os.path.abspath("../adda"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+project = 'adda'
+copyright = "2026, m-dml team"
+author = "m-dml team"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc.typehints",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "seed_intersphinx_mapping",
+    "sphinx_typo3_theme",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+add_module_names = True
+toc_object_entries_show_parents = "hide"
+templates_path = ["_templates"]
+exclude_patterns = ["../adda/_version.py"]
+autosummary_generate = True
 
 
 # -- Options for HTML output -------------------------------------------------
