67e6837ffac7ddc48525445026fe801a0cb47d2c
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2026-08-25 15:56:15 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import sys
 
-sys.path.insert(0, os.path.abspath("../adda"))
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
